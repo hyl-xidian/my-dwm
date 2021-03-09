da7b6db322374916794b188aeb95f456bcca6487
lhy <hyl.xidian@gmail.com>
2021-03-09 18:42:45 +0800

    change the default browser

--- config.h
+++ config.h
@@ -85,7 +85,7 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
-static const char *browsercmd[]  = { "firefox", NULL };
+static const char *browsercmd[]  = { "chromium", NULL };
 static const char *slockcmd[]  = { "slock", NULL };
 
 static const char *upvol[]   = { "/home/lhy/scripts/vol-up.sh",  NULL };
